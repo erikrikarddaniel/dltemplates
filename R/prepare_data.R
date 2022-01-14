4c61fc62eaa77a3318e66db335e90782e2787fc2
Daniel Lundin <matricaria.suaveolens@gmail.com>
2022-01-14 10:19:05 +0100

    Added prepare_data.R

---
+++ R/prepare_data.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+
+# __TITLE__
+#
+# __DESCRIPTION__
+#
+# Inputs:
+#
+# Outputs:
+#
+# Author: __AUTHOR_EMAIL__
+
+suppressPackageStartupMessages(library(readr))
+suppressPackageStartupMessages(library(arrow))
+suppressPackageStartupMessages(library(feather))
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(dtplyr))
+suppressPackageStartupMessages(library(dplyr, warn.conflicts = FALSE))
+suppressPackageStartupMessages(library(tidyr))
+suppressPackageStartupMessages(library(purrr))
+suppressPackageStartupMessages(library(stringr))
