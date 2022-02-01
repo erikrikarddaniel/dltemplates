76d3b4b13ef5862cbf97c94b53158439a9825989
Daniel Lundin <erik.rikard.daniel@gmail.com>
2022-02-01 08:10:15 +0100

    Updated setup script

--- setup_project.sh
+++ setup_project.sh
@@ -9,8 +9,11 @@ mkdir data
 touch data/.gitkeep
 mkdir scripts
 touch scripts/.gitkeep
+mkdir figures
+echo '*.png' >> figures/.gitignore
+echo '*.pdf' >> figures/.gitignore
 
-cp ~/dev/dltemplates/R/project.Rproj ${pname}.Rproj
+cp ~/dev/dltemplates/R/project.Rproj .
 sed "s/__PROJNAME__/${pname}/" ~/dev/dltemplates/misc/screenrc | sed '/__INSERTPOINT__/a chdir $ROOT/scripts\nscreen -t scripts' | sed '/__INSERTPOINT__/a chdir $ROOT/data\nscreen -t data' > .screenrc
 cp ~/dev/biomakefiles/gitignores/project_root.gitignore .gitignore
 cp ~/dev/dltemplates/R/rmarkdown.rmd ${pname}.Rmd
