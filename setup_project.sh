7836d986078dd7ab73d7d269b3e907f5921694ed
Daniel Lundin <erik.rikard.daniel@gmail.com>
2023-08-22 15:30:23 +0200

    Added basic Quarto template

--- setup_project.sh
+++ setup_project.sh
@@ -17,6 +17,7 @@ cp ~/dev/dltemplates/R/project.Rproj .
 sed "s/__PROJNAME__/${pname}/" ~/dev/dltemplates/misc/screenrc | sed '/__INSERTPOINT__/a chdir $ROOT/scripts\nscreen -t scripts' | sed '/__INSERTPOINT__/a chdir $ROOT/data\nscreen -t data' > .screenrc
 cp ~/dev/biomakefiles/gitignores/project_root.gitignore .gitignore
 cp ~/dev/dltemplates/R/rmarkdown.Rmd ${pname}.Rmd
+cp ~/dev/dltemplates/R/quarto.qmd ${pname}.qmd
 
 git init .
 git add .
