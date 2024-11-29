b25fce61d2676a65452c8d0e54cdce6ca4eb64e9
Daniel Lundin <erik.rikard.daniel@gmail.com>
2024-11-29 17:18:20 +0100

    Moved to tools.bib

--- setup_project.sh
+++ setup_project.sh
@@ -17,8 +17,10 @@ echo '*.pdf' >> figures/.gitignore
 cp $template_path/R/project.Rproj .
 sed "s/__PROJNAME__/${pname}/" $template_path/misc/screenrc | sed '/__INSERTPOINT__/a chdir $ROOT/scripts\nscreen -t scripts' | sed '/__INSERTPOINT__/a chdir $ROOT/data\nscreen -t data' > .screenrc
 cp $template_path/misc/gitignore .gitignore
-cp $template_path/misc/bibliography.bib .
+cp $template_path/misc/tools.bib .
 cp $template_path/R/quarto.qmd ${pname}.qmd
+
+touch bibliography.bib
 
 git init .
 git add .
