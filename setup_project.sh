b96ca87e5db645b47a67a3f9486a02bec51874ca
Daniel Lundin <matricaria.suaveolens@gmail.com>
2024-12-18 19:22:16 +0100

    Create pair of Makefiles

--- setup_project.sh
+++ setup_project.sh
@@ -14,6 +14,10 @@ mkdir figures
 echo '*.png' >> figures/.gitignore
 echo '*.pdf' >> figures/.gitignore
 
+echo 'all:' > Makefile
+echo '	cd data; make all' >> Makefile
+echo 'all:' > data/Makefile
+
 cp $template_path/R/project.Rproj .
 sed "s/__PROJNAME__/${pname}/" $template_path/misc/screenrc | sed '/__INSERTPOINT__/a chdir $ROOT/scripts\nscreen -t scripts' | sed '/__INSERTPOINT__/a chdir $ROOT/data\nscreen -t data' > .screenrc
 cp $template_path/misc/gitignore .gitignore
