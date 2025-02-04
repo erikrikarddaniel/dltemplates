2a9e15816a40194577c787f46a056eb05d22476d
Daniel Lundin <matricaria.suaveolens@gmail.com>
2025-02-04 10:10:45 +0100

    Reset indentation to 2

--- R/rsimple.r
+++ R/rsimple.r
@@ -20,44 +20,44 @@ options(warn = 1)
 
 # Get arguments
 option_list = list(
-    make_option(
-        c("-v", "--verbose"), action="store_true", default=FALSE, 
-        help="Print progress messages"
-    ),
-    make_option(
-        c("-V", "--version"), action="store_true", default=FALSE, 
-        help="Print program version and exit"
-    )
+  make_option(
+    c("-v", "--verbose"), action="store_true", default=FALSE, 
+    help="Print progress messages"
+  ),
+  make_option(
+    c("-V", "--version"), action="store_true", default=FALSE, 
+    help="Print program version and exit"
+  )
 )
 opt = parse_args(
-    OptionParser(
-        usage = "%prog [options] edger_result_file.tsv[.gz]\n\n\tThe EdgeR result file must contain a key to join in with the SEED tables, a 'contrast' column plus logFC, FDR, locCPM.", 
-        option_list = option_list
-    ), 
-    positional_arguments = TRUE
+  OptionParser(
+    usage = "%prog [options] edger_result_file.tsv[.gz]\n\n\tThe EdgeR result file must contain a key to join in with the SEED tables, a 'contrast' column plus logFC, FDR, locCPM.", 
+    option_list = option_list
+  ), 
+  positional_arguments = TRUE
 )
 
 if ( opt$options$version ) {
-    write(SCRIPT_VERSION, stdout())
-    quit('no')
+  write(SCRIPT_VERSION, stdout())
+  quit('no')
 }
 
 DEBUG   = 0
 INFO    = 1
 WARNING = 2
 LOG_LEVELS = list(
-    DEBUG   = list(n = 0, msg = 'DEBUG'),
-    INFO    = list(n = 1, msg = 'INFO'),
-    WARNING = list(n = 2, msg = 'WARNING'),
-    ERROR   = list(n = 3, msg = 'ERROR')
+  DEBUG   = list(n = 0, msg = 'DEBUG'),
+  INFO    = list(n = 1, msg = 'INFO'),
+  WARNING = list(n = 2, msg = 'WARNING'),
+  ERROR   = list(n = 3, msg = 'ERROR')
 )
 logmsg    = function(msg, llevel='INFO') {
-    if ( opt$options$verbose | LOG_LEVELS[[llevel]][["n"]] >= LOG_LEVELS[["INFO"]][["n"]] ) {
-        write(
-            sprintf("%s: %s: %s", llevel, format(Sys.time(), "%Y-%m-%d %H:%M:%S"), msg),
-            stderr()
-        )
-    }
+  if ( opt$options$verbose | LOG_LEVELS[[llevel]][["n"]] >= LOG_LEVELS[["INFO"]][["n"]] ) {
+    write(
+      sprintf("%s: %s: %s", llevel, format(Sys.time(), "%Y-%m-%d %H:%M:%S"), msg),
+      stderr()
+    )
+  }
 }
 logmsg(sprintf("Reading %s", opt$inputfile))
 
